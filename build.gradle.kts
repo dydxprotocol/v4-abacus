413fe3571a50d5110073ae7caf317ed2845b7cc0
Rui <rui@dydx.exchange>
2023-08-22 11:56:43 -0700

    Fix parsing issue of subaccounts and balances overwritten.

--- build.gradle.kts
+++ build.gradle.kts
@@ -30,7 +30,7 @@ plugins {
 }
 
 group = "exchange.dydx.abacus"
-version = "0.2.32"
+version = "0.2.33"
 
 repositories {
     google()
--- integration/iOS/Pods/Target Support Files/abacus/abacus-copy-dsyms.sh
+++ integration/iOS/Pods/Target Support Files/abacus/abacus-copy-dsyms.sh
@@ -83,4 +83,4 @@ install_bcsymbolmap() {
     rsync --delete -av "${RSYNC_PROTECT_TMP_FILES[@]}" --filter "- CVS/" --filter "- .svn/" --filter "- .git/" --filter "- .hg/" --filter "- Headers" --filter "- PrivateHeaders" --filter "- Modules" "${bcsymbolmap_path}" "${destination}"
 }
 
-install_dsym "${PODS_ROOT}/../../../build/cocoapods/framework/Abacus.framework.dSYM"
+install_dsym "${PODS_ROOT}/../../../../abacus/build/cocoapods/framework/Abacus.framework.dSYM"
