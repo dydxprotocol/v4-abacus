bc3b918b7d85e05d6ee016f6a83f8fbfd205159e
Rui <rui@dydx.exchange>
2023-09-18 09:40:27 -0700

    ./gradlew kotlinUpgradeYarnLock

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,7 +9,7 @@ buildscript {
         mavenCentral()
     }
     dependencies {
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.8.22")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.10")
         classpath("com.android.tools.build:gradle:$agp_version")
     }
 }
