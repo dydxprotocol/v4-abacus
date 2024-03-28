f3deced5078f98e715fb30ca22e3b628e5ba119c
mike-dydx <mike@dydx.exchange>
2024-03-28 11:43:46 -0400

    Update TradingStateMachine+TransferInput.kt

--- src/commonMain/kotlin/exchange.dydx.abacus/state/model/TradingStateMachine+TransferInput.kt
+++ src/commonMain/kotlin/exchange.dydx.abacus/state/model/TradingStateMachine+TransferInput.kt
@@ -11,8 +11,6 @@ import exchange.dydx.abacus.utils.safeSet
 import kollections.JsExport
 import kollections.iListOf
 import kotlinx.serialization.Serializable
-import kotlinx.serialization.json.Json
-import kotlinx.serialization.json.jsonObject
 
 @JsExport
 @Serializable
