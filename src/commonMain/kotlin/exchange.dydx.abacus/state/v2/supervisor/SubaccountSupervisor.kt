64f3692a9f6e0682502afd781fcebb042c51883d
moo-onthelawn <70078372+moo-onthelawn@users.noreply.github.com>
2024-04-04 12:12:39 -0400

    1.6.30: Fix commitTriggerOrders payload return type (#289)
    * fix
    
    * update it
    
    * bump version
    
    * good til block

--- src/commonMain/kotlin/exchange.dydx.abacus/state/v2/supervisor/SubaccountSupervisor.kt
+++ src/commonMain/kotlin/exchange.dydx.abacus/state/v2/supervisor/SubaccountSupervisor.kt
@@ -658,9 +658,9 @@ internal class SubaccountSupervisor(
     ): HumanReadableTriggerOrdersPayload {
         val payloads = triggerOrdersPayload(currentHeight)
 
-        payloads.cancelOrderPayloads.forEach {
-            val string = Json.encodeToString(it)
-            val analyticsPayload = analyticsUtils.formatCancelOrderPayload(it, true)
+        payloads.cancelOrderPayloads.forEach { orderPayload ->
+            val string = Json.encodeToString(orderPayload)
+            val analyticsPayload = analyticsUtils.formatCancelOrderPayload(orderPayload, true)
 
             val uiClickTimeMs = Clock.System.now().toEpochMilliseconds().toDouble()
             tracking(AnalyticsEvent.TradeCancelOrderClick.rawValue, analyticsPayload)
@@ -673,17 +673,17 @@ internal class SubaccountSupervisor(
                         ParsingHelper.merge(uiTrackingParmas(uiDelayTimeMs), analyticsPayload)?.toIMap(),
                     )
                     helper.ioImplementations.threading?.async(ThreadingType.abacus) {
-                        this.orderCanceled(it.orderId)
+                        this.orderCanceled(orderPayload.orderId)
                         this.cancelOrderRecords.add(
                             CancelOrderRecord(
                                 subaccountNumber,
-                                it.clientId,
+                                orderPayload.clientId,
                                 submitTimeMs,
                             ),
                         )
                     }
                 }
-                helper.send(error, callback, it)
+                helper.send(error, callback, HumanReadableTriggerOrdersPayload(emptyList(), listOf(orderPayload)))
             }
 
             transactionQueue.enqueue(
@@ -691,12 +691,12 @@ internal class SubaccountSupervisor(
             )
         }
 
-        payloads.placeOrderPayloads.forEach {
-            val clientId = it.clientId
-            val string = Json.encodeToString(it)
+        payloads.placeOrderPayloads.forEach { orderPayload ->
+            val clientId = orderPayload.clientId
+            val string = Json.encodeToString(orderPayload)
 
             val analyticsPayload = analyticsUtils.formatPlaceOrderPayload(
-                it,
+                orderPayload,
                 false,
                 true,
             )
@@ -717,14 +717,14 @@ internal class SubaccountSupervisor(
                         this.placeOrderRecords.add(
                             PlaceOrderRecord(
                                 subaccountNumber,
-                                it.clientId,
+                                orderPayload.clientId,
                                 submitTimeMs,
                             ),
                         )
                         lastOrderClientId = clientId
                     }
                 }
-                helper.send(error, callback, it)
+                helper.send(error, callback, HumanReadableTriggerOrdersPayload(listOf(orderPayload), emptyList()))
             }
 
             transactionQueue.enqueue(
@@ -900,6 +900,7 @@ internal class SubaccountSupervisor(
 
         val duration = GoodTil.duration(TradeInputGoodUntil(TRIGGER_ORDER_DEFAULT_DURATION_DAYS, "D")) ?: throw Exception("invalid duration")
         val goodTilTimeInSeconds = (duration / 1.seconds).toInt()
+        val goodTilBlock = null
 
         val marketInfo = marketInfo(marketId)
 
@@ -917,6 +918,7 @@ internal class SubaccountSupervisor(
             timeInForce,
             execution,
             goodTilTimeInSeconds,
+            goodTilBlock,
             marketInfo,
             currentHeight,
         )
@@ -1048,7 +1050,7 @@ internal class SubaccountSupervisor(
         val clientId = order.clientId ?: error("clientId is null")
         val orderFlags = order.orderFlags ?: error("orderFlags is null")
         val clobPairId = order.clobPairId ?: error("clobPairId is null")
-        val orderSubaccountNumber = order.subaccountNumber?: error("order subaccountNumber is null")
+        val orderSubaccountNumber = order.subaccountNumber ?: error("order subaccountNumber is null")
         val goodTilBlock = order.goodTilBlock
         val goodTilBlockTime = order.goodTilBlockTime
 
