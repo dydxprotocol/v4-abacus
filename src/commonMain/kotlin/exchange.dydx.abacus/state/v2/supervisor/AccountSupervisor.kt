5d3d27d69d5ac107553ec6e5c2f9eb4b632ca27e
Bill <bill@dydx.exchange>
2024-06-20 14:27:13 -0700

    Compliance: Poll geo, deprecate ComplianceAction.Onboard (#458)
    Co-authored-by: mobile-build-bot-git <mobile-build-bot-git@dydx.exchange>

--- src/commonMain/kotlin/exchange.dydx.abacus/state/v2/supervisor/AccountSupervisor.kt
+++ src/commonMain/kotlin/exchange.dydx.abacus/state/v2/supervisor/AccountSupervisor.kt
@@ -208,6 +208,7 @@ internal open class AccountSupervisor(
 
     init {
         screenAccountAddress()
+        complianceScreen(DydxAddress(accountAddress), ComplianceAction.CONNECT)
     }
 
     internal fun subscribeToSubaccount(subaccountNumber: Int) {
@@ -331,9 +332,6 @@ internal open class AccountSupervisor(
                     val isValidResponse = helper.success(httpCode) && response != null
                     if (isValidResponse) {
                         response?.let { retrievedSubaccounts(it) }
-                        complianceScreen(DydxAddress(accountAddress), ComplianceAction.CONNECT)
-                    } else {
-                        complianceScreen(DydxAddress(accountAddress), ComplianceAction.ONBOARD)
                     }
                     if (!isValidResponse && httpCode != 403) {
                         subaccountNumber = 0
@@ -817,12 +815,15 @@ internal open class AccountSupervisor(
         }
     }
 
-    internal open fun triggerCompliance(action: ComplianceAction, callback: TransactionCallback) {
+    internal open fun triggerCompliance(action: ComplianceAction, callback: TransactionCallback?) {
         if (compliance.status != ComplianceStatus.UNKNOWN) {
             updateCompliance(DydxAddress(accountAddress), compliance.status, action)
-            callback(true, null, null)
+            if (callback != null) {
+                callback(true, null, null)
+            }
+        } else if (callback != null) {
+            callback(false, V4TransactionErrors.error(null, "No account address"), null)
         }
-        callback(false, V4TransactionErrors.error(null, "No account address"), null)
     }
 
     private fun complianceScreenUrl(address: String): String? {
