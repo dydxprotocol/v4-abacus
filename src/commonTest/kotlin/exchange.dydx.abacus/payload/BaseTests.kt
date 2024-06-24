76bb6b5e8709cd1a6b1ff754b7b7eb65f1b99ebc
Rui <rui@dydx.exchange>
2024-06-24 10:00:00 -0700

    Fix tests

--- src/commonTest/kotlin/exchange.dydx.abacus/payload/BaseTests.kt
+++ src/commonTest/kotlin/exchange.dydx.abacus/payload/BaseTests.kt
@@ -1114,11 +1114,6 @@ open class BaseTests(private val maxSubaccountNumber: Int, internal val useParen
                 obj.equity,
                 "$trace.equity",
             )
-            verifyDoubleValues(
-                parser.asNativeMap(data["marginValue"]),
-                obj.marginValue,
-                "$trace.marginValue",
-            )
         } else {
             assertNull(obj)
         }
