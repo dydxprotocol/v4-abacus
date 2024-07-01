5a2debd135ad54da204b3c6e2deef93691a355e2
Bill He <bill@dydx.exchange>
2024-07-01 09:48:03 -0700

    fix: cancel nobleBalancesTimer when cosmosWalletConnected is true

--- src/commonMain/kotlin/exchange.dydx.abacus/state/v2/supervisor/AccountSupervisor.kt
+++ src/commonMain/kotlin/exchange.dydx.abacus/state/v2/supervisor/AccountSupervisor.kt
@@ -118,7 +118,13 @@ internal open class AccountSupervisor(
         }
 
     var cosmosWalletConnected: Boolean? = false
-
+        internal set(value) {
+            field = value
+            if (value == true) {
+                nobleBalancesTimer?.cancel()
+                nobleBalancesTimer = null
+            }
+        }
     private var sourceAddressRestriction: Restriction? = null
         set(value) {
             if (field != value) {
@@ -439,6 +445,7 @@ internal open class AccountSupervisor(
 
     private fun retrieveNobleBalance() {
         if (cosmosWalletConnected == true) {
+            nobleBalancesTimer = null
             return
         }
         val timer = helper.ioImplementations.timer ?: CoroutineTimer.instance
