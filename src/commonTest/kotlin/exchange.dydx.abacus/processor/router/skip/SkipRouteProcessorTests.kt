dec1cab6662006a78d10493f612f02bd9a4ce77c
jeremy lee <jeremy@dydx.exchange>
2024-07-15 17:22:33 -0400

    fix tests

--- src/commonTest/kotlin/exchange.dydx.abacus/processor/router/skip/SkipRouteProcessorTests.kt
+++ src/commonTest/kotlin/exchange.dydx.abacus/processor/router/skip/SkipRouteProcessorTests.kt
@@ -105,14 +105,14 @@ class SkipRouteProcessorTests {
             "timeoutTimestamp" to 1718399715601228463,
             "memo" to "{\"forward\":{\"channel\":\"channel-1\",\"next\":{\"wasm\":{\"contract\":\"osmo1vkdakqqg5htq5c3wy2kj2geq536q665xdexrtjuwqckpads2c2nsvhhcyv\",\"msg\":{\"swap_and_action\":{\"affiliates\":[],\"min_asset\":{\"native\":{\"amount\":\"37656643372307734\",\"denom\":\"ibc/EA1D43981D5C9A1C4AAEA9C23BB1D4FA126BA9BC7020A25E0AE4AA841EA25DC5\"}},\"post_swap_action\":{\"ibc_transfer\":{\"ibc_info\":{\"memo\":\"{\\\"destination_chain\\\":\\\"Ethereum\\\",\\\"destination_address\\\":\\\"0xD397883c12b71ea39e0d9f6755030205f31A1c96\\\",\\\"payload\\\":[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,15,120,51,119,123,252,158,247,45,139,118,174,149,77,56,73,221,113,248,41],\\\"type\\\":2,\\\"fee\\\":{\\\"amount\\\":\\\"7692677672185391\\\",\\\"recipient\\\":\\\"axelar1aythygn6z5thymj6tmzfwekzh05ewg3l7d6y89\\\"}}\",\"receiver\":\"axelar1dv4u5k73pzqrxlzujxg3qp8kvc3pje7jtdvu72npnt5zhq05ejcsn5qme5\",\"recover_address\":\"osmo1nhzuazjhyfu474er6v4ey8zn6wa5fy6gt044g4\",\"source_channel\":\"channel-208\"}}},\"timeout_timestamp\":1718399715601274600,\"user_swap\":{\"swap_exact_asset_in\":{\"operations\":[{\"denom_in\":\"ibc/498A0751C798A0D9A389AA3691123DADA57DAA4FE165D5C75894505B876BA6E4\",\"denom_out\":\"factory/osmo1z0qrq605sjgcqpylfl4aa6s90x738j7m58wyatt0tdzflg2ha26q67k743/wbtc\",\"pool\":\"1437\"},{\"denom_in\":\"factory/osmo1z0qrq605sjgcqpylfl4aa6s90x738j7m58wyatt0tdzflg2ha26q67k743/wbtc\",\"denom_out\":\"ibc/EA1D43981D5C9A1C4AAEA9C23BB1D4FA126BA9BC7020A25E0AE4AA841EA25DC5\",\"pool\":\"1441\"}],\"swap_venue_name\":\"osmosis-poolmanager\"}}}}}},\"port\":\"transfer\",\"receiver\":\"osmo1vkdakqqg5htq5c3wy2kj2geq536q665xdexrtjuwqckpads2c2nsvhhcyv\",\"retries\":2,\"timeout\":1718399715601230847}}",
         )
-        val expectedData = jsonEncoder.encode(
+        val expectedDataRaw =
             mapOf(
                 "msg" to expectedMsg,
                 "value" to expectedMsg,
                 "msgTypeUrl" to "/ibc.applications.transfer.v1.MsgTransfer",
                 "typeUrl" to "/ibc.applications.transfer.v1.MsgTransfer",
-            ),
-        )
+            )
+
         val expected = mapOf(
             "toAmountUSD" to 103.17,
             "toAmount" to 0.03034433583519616,
@@ -124,7 +124,8 @@ class SkipRouteProcessorTests {
                 "fromAddress" to "ibc/8E27BA2D5493AF5636760E354E46004562C46AB7EC0CC4C1CA14E9E20E2545B5",
                 "toChainId" to "1",
                 "toAddress" to "ethereum-native",
-                "data" to expectedData,
+                "data" to jsonEncoder.encode(expectedDataRaw),
+                "allMessagesArray" to jsonEncoder.encode(listOf(expectedDataRaw)),
             ),
         )
 
@@ -223,13 +224,11 @@ class SkipRouteProcessorTests {
             "timeoutHeight" to mapOf<String, Any>(),
             "timeoutTimestamp" to 1718318348813666048,
         )
-        val expectedData = jsonEncoder.encode(
-            mapOf(
-                "msg" to expectedMsg,
-                "value" to expectedMsg,
-                "msgTypeUrl" to "/ibc.applications.transfer.v1.MsgTransfer",
-                "typeUrl" to "/ibc.applications.transfer.v1.MsgTransfer",
-            ),
+        val expectedDataRaw = mapOf(
+            "msg" to expectedMsg,
+            "value" to expectedMsg,
+            "msgTypeUrl" to "/ibc.applications.transfer.v1.MsgTransfer",
+            "typeUrl" to "/ibc.applications.transfer.v1.MsgTransfer",
         )
         val expected = mapOf(
             "toAmountUSD" to 0.01,
@@ -241,7 +240,8 @@ class SkipRouteProcessorTests {
                 "fromAddress" to "uusdc",
                 "toChainId" to "dydx-mainnet-1",
                 "toAddress" to "ibc/8E27BA2D5493AF5636760E354E46004562C46AB7EC0CC4C1CA14E9E20E2545B5",
-                "data" to expectedData,
+                "data" to jsonEncoder.encode(expectedDataRaw),
+                "allMessagesArray" to jsonEncoder.encode(listOf(expectedDataRaw)),
             ),
         )
         assertEquals(expected, result)
