5be03b0285d6f0323481866584d7bc1ff4b4e2df
Prashan Dharmasena <prashan@dydx.exchange>
2024-07-17 14:28:04 +0200

    Followups for static typing for trades messages.

--- src/commonMain/kotlin/exchange.dydx.abacus/processor/markets/TradeProcessorV2.kt
+++ src/commonMain/kotlin/exchange.dydx.abacus/processor/markets/TradeProcessorV2.kt
@@ -6,14 +6,12 @@ import exchange.dydx.abacus.output.input.OrderSide
 import exchange.dydx.abacus.output.input.OrderType
 import exchange.dydx.abacus.processor.base.mergeWithIds
 import exchange.dydx.abacus.protocols.LocalizerProtocol
+import exchange.dydx.abacus.protocols.ParserProtocol
 import exchange.dydx.abacus.utils.IndexerResponseParsingException
 import exchange.dydx.abacus.utils.Logger
 import exchange.dydx.abacus.utils.parseException
-import exchange.dydx.abacus.utils.toJson
 import indexer.codegen.IndexerTradeResponseObject
 import kotlinx.serialization.Serializable
-import kotlinx.serialization.SerializationException
-import kotlinx.serialization.json.Json
 
 @Serializable
 data class TradesResponse(
@@ -25,53 +23,34 @@ internal class TradesProcessorV2(
     private val limit: Int = TRADES_LIMIT,
 ) {
     fun processSubscribed(
-        content: Map<String, Any>
+        payload: TradesResponse
     ): List<MarketTrade> {
-        return try {
-            val payloadJson = content.toJson()
-            val trades = Json.decodeFromString<TradesResponse>(payloadJson)
-            trades.trades.mapNotNull {
-                tradeProcessor.process(it)
-            }
-        } catch (e: SerializationException) {
-            Logger.e { "failed to decode: $content" }
-            emptyList()
-        } catch (e: IllegalArgumentException) {
-            Logger.e { "failed to decode: $content" }
-            emptyList()
+        return payload.trades.mapNotNull {
+            tradeProcessor.process(it)
         }
     }
 
     fun processChannelData(
         existing: List<MarketTrade>?,
-        content: Map<String, Any>,
+        payload: TradesResponse,
     ): List<MarketTrade> {
-        return try {
-            val payloadJson = content.toJson()
-            val trades = Json.decodeFromString<TradesResponse>(payloadJson)
-            val new = trades.trades.mapNotNull {
-                tradeProcessor.process(it)
-            }
-            val merged = existing?.let {
-                mergeWithIds(new, existing) { trade -> trade.id }
-            } ?: new
+        val new = payload.trades.mapNotNull {
+            tradeProcessor.process(it)
+        }
+        val merged = existing?.let {
+            mergeWithIds(new, existing) { trade -> trade.id }
+        } ?: new
 
-            if (merged.size > limit) {
-                merged.subList(0, limit)
-            } else {
-                merged
-            }
-        } catch (e: SerializationException) {
-            Logger.e { "failed to decode: $content" }
-            existing ?: emptyList()
-        } catch (e: IllegalArgumentException) {
-            Logger.e { "failed to decode: $content" }
-            existing ?: emptyList()
+        return if (merged.size > limit) {
+            merged.subList(0, limit)
+        } else {
+            merged
         }
     }
 }
 
 internal class TradeProcessorV2(
+    private val parser: ParserProtocol,
     private val localizer: LocalizerProtocol?,
 ) {
     fun process(payload: IndexerTradeResponseObject): MarketTrade? {
@@ -81,8 +60,8 @@ internal class TradeProcessorV2(
                 side = payload.side?.name?.let { OrderSide(it) } ?: parseException(payload),
                 size = payload.size?.toDouble() ?: parseException(payload),
                 price = payload.price?.toDouble() ?: parseException(payload),
-                type = payload.type?.name?.let { OrderType(it) } ?: parseException(payload),
-                createdAtMilliseconds = payload.createdAt?.toDouble() ?: parseException(payload),
+                type = payload.type?.name?.let { OrderType(it) },
+                createdAtMilliseconds = payload.createdAt?.let { parser.asDatetime(it)?.toEpochMilliseconds()?.toDouble() } ?: parseException(payload),
                 resources = run {
                     val key = payload.side?.name?.let { sideStringKeys[it] } ?: parseException(payload)
                     val string = localizer?.localize(key)
