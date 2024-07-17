1f6fc119e572d9743b1636026ef7bbd976781515
mike-dydx <mike@dydx.exchange>
2024-07-17 15:48:49 -0400

    force gas price and gas limit in skip processor

--- src/commonMain/kotlin/exchange.dydx.abacus/utils/Constants.kt
+++ src/commonMain/kotlin/exchange.dydx.abacus/utils/Constants.kt
@@ -26,3 +26,7 @@ internal const val GEO_POLLING_DURATION_SECONDS = 10.0
 
 // Autosweep Constants
 internal const val MIN_USDC_AMOUNT_FOR_AUTO_SWEEP = 20000
+
+// Gas Constants based on historical Squid responses
+internal const val DEFAULT_GAS_LIMIT = 1500000
+internal const val DEFAULT_GAS_PRICE = 1520000000
