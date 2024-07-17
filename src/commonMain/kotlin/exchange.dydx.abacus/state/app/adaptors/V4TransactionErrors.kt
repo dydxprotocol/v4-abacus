3b5fbaa4b878652edfd635828ae059bce560b1f0
aforaleka <aforaleka@gmail.com>
2024-07-17 12:44:51 -0400

    parse query result error for subaccount update

--- src/commonMain/kotlin/exchange.dydx.abacus/state/app/adaptors/V4TransactionErrors.kt
+++ src/commonMain/kotlin/exchange.dydx.abacus/state/app/adaptors/V4TransactionErrors.kt
@@ -16,9 +16,46 @@ class V4TransactionErrors {
                 } else {
                     null
                 }
+            } else if (message?.startsWith("Query failed") == true) {
+                parseQueryResultErrorFromMessage(message)
             } else {
                 ParsingError(ParsingErrorType.BackendError, message ?: "Unknown error", null)
             }
+        }
+
+        private fun parseQueryResultErrorFromMessage(message: String): ParsingError {
+            // Workaround: Regex match different query results until protocol can return codespace/code
+            parseSubaccountUpdateError(message)?.let { return it }
+            return ParsingError(ParsingErrorType.BackendError, "Unknown query result error", null)
+        }
+
+        private fun parseSubaccountUpdateError(message: String): ParsingError? {
+            val failedSubaccountUpdateResultPattern = Regex("""Subaccount with id \{[^}]+\} failed with UpdateResult:\s*([A-Za-z]+):""")
+            val matchResult = failedSubaccountUpdateResultPattern.find(message)
+            return matchResult?.groups?.get(1)?.value?.let {
+                val matchedUpdateResult = SubaccountUpdateFailedResult.invoke(it)
+                ParsingError(
+                    ParsingErrorType.BackendError,
+                    "Subaccount update error: $matchedUpdateResult",
+                    "ERRORS.QUERY_ERROR_SUBACCOUNTS_$matchedUpdateResult",
+                )
+            }
+        }
+    }
+}
+
+
+// Copied from protocol subaccounts update (https://github.com/dydxprotocol/v4-chain/blob/b2dfda2a4b0ea587691c41ba436b46d0d9987a25/protocol/x/subaccounts/types/update.go#L58)
+enum class SubaccountUpdateFailedResult(val rawValue: String) {
+    NewlyUndercollateralized("NewlyUndercollateralized"),
+    StillUndercollateralized("StillUndercollateralized"),
+    WithdrawalsAndTransfersBlocked("WithdrawalsAndTransfersBlocked"),
+    UpdateCausedError("UpdateCausedError"),
+    ViolatesIsolatedSubaccountConstraints("ViolatesIsolatedSubaccountConstraints");
+
+    companion object {
+        fun invoke(rawValue: String): SubaccountUpdateFailedResult? {
+            return SubaccountUpdateFailedResult.values().find { it.rawValue == rawValue }
         }
     }
 }
