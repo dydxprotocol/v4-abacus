13df011df6a6f15a65977d63d54eb1a6cbb527b3
Rui <102453770+ruixhuang@users.noreply.github.com>
2024-08-28 21:47:51 +0800

    Save remote localization files to cache (#606)
    Co-authored-by: mobile-build-bot-git <mobile-build-bot-git@dydx.exchange>

--- src/commonMain/kotlin/exchange.dydx.abacus/state/app/helper/DynamicLocalizer.kt
+++ src/commonMain/kotlin/exchange.dydx.abacus/state/app/helper/DynamicLocalizer.kt
@@ -183,6 +183,7 @@ class DynamicLocalizer(
             ioImplementations.rest?.get(url, null) { response, code, _ ->
                 ioImplementations.threading?.async(ThreadingType.main) {
                     if (code in 200..299 && response != null) {
+                        ioImplementations.fileSystem?.writeTextFile("$path/$file", response)
                         val data = parser.decodeJsonObject(response)
                         if (data != null && data.size != 0) {
                             mergeLanguageData(data, file, language)
