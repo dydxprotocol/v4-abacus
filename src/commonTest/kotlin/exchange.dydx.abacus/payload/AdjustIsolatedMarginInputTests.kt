b2981c91b7619cc0be8d042d31a9e7c67b83b12e
Rui <rui@dydx.exchange>
2024-08-28 09:23:47 +0800

    More tests

--- src/commonTest/kotlin/exchange.dydx.abacus/payload/AdjustIsolatedMarginInputTests.kt
+++ src/commonTest/kotlin/exchange.dydx.abacus/payload/AdjustIsolatedMarginInputTests.kt
@@ -3,6 +3,7 @@ package exchange.dydx.abacus.payload.v4
 import exchange.dydx.abacus.calculator.CalculationPeriod
 import exchange.dydx.abacus.output.input.InputType
 import exchange.dydx.abacus.output.input.IsolatedMarginAdjustmentType
+import exchange.dydx.abacus.output.input.IsolatedMarginInputType
 import exchange.dydx.abacus.responses.StateResponse
 import exchange.dydx.abacus.state.app.adaptors.AbUrl
 import exchange.dydx.abacus.state.model.AdjustIsolatedMarginInputField
@@ -59,7 +60,7 @@ class AdjustIsolatedMarginInputTests : V4BaseTests(useParentSubaccount = true) {
             perp.adjustIsolatedMargin(
                 data = "128",
                 type = AdjustIsolatedMarginInputField.ChildSubaccountNumber,
-                parentSubaccountNumber = 0
+                parentSubaccountNumber = 0,
             )
 
             assertEquals(InputType.ADJUST_ISOLATED_MARGIN, perp.internalState.input.currentType)
@@ -70,7 +71,7 @@ class AdjustIsolatedMarginInputTests : V4BaseTests(useParentSubaccount = true) {
                     perp.adjustIsolatedMargin(
                         "128",
                         AdjustIsolatedMarginInputField.ChildSubaccountNumber,
-                        0
+                        0,
                     )
                 },
                 """
@@ -82,7 +83,7 @@ class AdjustIsolatedMarginInputTests : V4BaseTests(useParentSubaccount = true) {
                     }
                 }
             }
-            """.trimIndent(),
+                """.trimIndent(),
             )
         }
     }
@@ -92,7 +93,7 @@ class AdjustIsolatedMarginInputTests : V4BaseTests(useParentSubaccount = true) {
             perp.adjustIsolatedMargin(
                 data = "ETH-USD",
                 type = AdjustIsolatedMarginInputField.Market,
-                parentSubaccountNumber = 0
+                parentSubaccountNumber = 0,
             )
 
             assertEquals(InputType.ADJUST_ISOLATED_MARGIN, perp.internalState.input.currentType)
@@ -111,7 +112,7 @@ class AdjustIsolatedMarginInputTests : V4BaseTests(useParentSubaccount = true) {
                     }
                 }
             }
-            """.trimIndent(),
+                """.trimIndent(),
             )
         }
     }
@@ -121,15 +122,15 @@ class AdjustIsolatedMarginInputTests : V4BaseTests(useParentSubaccount = true) {
             perp.adjustIsolatedMargin(
                 data = "0",
                 type = AdjustIsolatedMarginInputField.Amount,
-                parentSubaccountNumber = 0
+                parentSubaccountNumber = 0,
             )
 
             assertEquals(InputType.ADJUST_ISOLATED_MARGIN, perp.internalState.input.currentType)
             assertEquals(0.0, perp.internalState.input.adjustIsolatedMargin.amount)
 
-            val subaccount = perp.internalState.wallet.account.groupedSubaccounts[0]
+            val subaccount = perp.internalState.wallet.account.subaccounts[0]
             assertEquals(100000.0, subaccount?.calculated?.get(CalculationPeriod.current)?.quoteBalance)
-            val subaccount1 = perp.internalState.wallet.account.groupedSubaccounts[128]
+            val subaccount1 = perp.internalState.wallet.account.subaccounts[128]
             assertEquals(500.0, subaccount1?.calculated?.get(CalculationPeriod.current)?.quoteBalance)
         } else {
             test(
@@ -163,7 +164,7 @@ class AdjustIsolatedMarginInputTests : V4BaseTests(useParentSubaccount = true) {
                     }
                 }
             }
-            """.trimIndent(),
+                """.trimIndent(),
             )
         }
     }
@@ -173,7 +174,7 @@ class AdjustIsolatedMarginInputTests : V4BaseTests(useParentSubaccount = true) {
             perp.adjustIsolatedMargin(
                 data = IsolatedMarginAdjustmentType.Add.name,
                 type = AdjustIsolatedMarginInputField.Type,
-                parentSubaccountNumber = 0
+                parentSubaccountNumber = 0,
             )
 
             assertEquals(InputType.ADJUST_ISOLATED_MARGIN, perp.internalState.input.currentType)
@@ -185,7 +186,7 @@ class AdjustIsolatedMarginInputTests : V4BaseTests(useParentSubaccount = true) {
                     perp.adjustIsolatedMargin(
                         IsolatedMarginAdjustmentType.Add.name,
                         AdjustIsolatedMarginInputField.Type,
-                        0
+                        0,
                     )
                 },
                 """
@@ -202,33 +203,27 @@ class AdjustIsolatedMarginInputTests : V4BaseTests(useParentSubaccount = true) {
                 }
             }
         }
-            """.trimIndent(),
+                """.trimIndent(),
             )
         }
 
-        test(
-            {
-                perp.adjustIsolatedMargin("92.49", AdjustIsolatedMarginInputField.Amount, 0)
-            },
-            """
-            {
-                "input": {
-                    "current": "adjustIsolatedMargin",
-                    "adjustIsolatedMargin": {
-                        "Amount": "92.49",
-                        "AmountInput": "Amount",
-                        "AmountPercent": "0.0013"
-                    }
-                }
-            }
-            """.trimIndent(),
-        )
+        if (perp.staticTyping) {
+            perp.adjustIsolatedMargin(
+                data = "92.49",
+                type = AdjustIsolatedMarginInputField.Amount,
+                parentSubaccountNumber = 0,
+            )
 
-        test(
-            {
-                perp.adjustIsolatedMargin("-92.49", AdjustIsolatedMarginInputField.Amount, 0)
-            },
-            """
+            assertEquals(InputType.ADJUST_ISOLATED_MARGIN, perp.internalState.input.currentType)
+            assertEquals(92.49, perp.internalState.input.adjustIsolatedMargin.amount)
+            assertEquals(IsolatedMarginInputType.Amount, perp.internalState.input.adjustIsolatedMargin.amountInput)
+            assertEquals(0.0013086578949669525, perp.internalState.input.adjustIsolatedMargin.amountPercent)
+        } else {
+            test(
+                {
+                    perp.adjustIsolatedMargin("92.49", AdjustIsolatedMarginInputField.Amount, 0)
+                },
+                """
             {
                 "input": {
                     "current": "adjustIsolatedMargin",
@@ -239,16 +234,68 @@ class AdjustIsolatedMarginInputTests : V4BaseTests(useParentSubaccount = true) {
                     }
                 }
             }
-            """.trimIndent(),
-        )
+                """.trimIndent(),
+            )
+        }
+
+        if (perp.staticTyping) {
+            perp.adjustIsolatedMargin(
+                data = "-92.49",
+                type = AdjustIsolatedMarginInputField.Amount,
+                parentSubaccountNumber = 0,
+            )
+
+            assertEquals(InputType.ADJUST_ISOLATED_MARGIN, perp.internalState.input.currentType)
+            assertEquals(92.49, perp.internalState.input.adjustIsolatedMargin.amount)
+            assertEquals(IsolatedMarginInputType.Amount, perp.internalState.input.adjustIsolatedMargin.amountInput)
+            assertEquals(0.0013086578949669525, perp.internalState.input.adjustIsolatedMargin.amountPercent)
+        } else {
+            test(
+                {
+                    perp.adjustIsolatedMargin("-92.49", AdjustIsolatedMarginInputField.Amount, 0)
+                },
+                """
+            {
+                "input": {
+                    "current": "adjustIsolatedMargin",
+                    "adjustIsolatedMargin": {
+                        "Amount": "92.49",
+                        "AmountInput": "Amount",
+                        "AmountPercent": "0.0013"
+                    }
+                }
+            }
+                """.trimIndent(),
+            )
+        }
     }
 
     private fun testMarginAmountRemoval() {
-        test(
-            {
-                perp.adjustIsolatedMargin(IsolatedMarginAdjustmentType.Remove.name, AdjustIsolatedMarginInputField.Type, 0)
-            },
-            """
+        if (perp.staticTyping) {
+            perp.adjustIsolatedMargin(
+                data = IsolatedMarginAdjustmentType.Remove.name,
+                type = AdjustIsolatedMarginInputField.Type,
+                parentSubaccountNumber = 0,
+            )
+
+            assertEquals(InputType.ADJUST_ISOLATED_MARGIN, perp.internalState.input.currentType)
+            assertEquals(IsolatedMarginAdjustmentType.Remove, perp.internalState.input.adjustIsolatedMargin.type)
+            assertEquals(70675.46098618512, perp.internalState.input.adjustIsolatedMargin.summary?.crossFreeCollateral)
+
+            val subaccount = perp.internalState.wallet.account.subaccounts[0]
+            assertEquals(100000.0, subaccount?.calculated?.get(CalculationPeriod.current)?.quoteBalance)
+            val subaccount1 = perp.internalState.wallet.account.subaccounts[128]
+            assertEquals(500.0, subaccount1?.calculated?.get(CalculationPeriod.current)?.quoteBalance)
+        } else {
+            test(
+                {
+                    perp.adjustIsolatedMargin(
+                        IsolatedMarginAdjustmentType.Remove.name,
+                        AdjustIsolatedMarginInputField.Type,
+                        0,
+                    )
+                },
+                """
             {
                 "input": {
                     "current": "adjustIsolatedMargin",
@@ -278,14 +325,34 @@ class AdjustIsolatedMarginInputTests : V4BaseTests(useParentSubaccount = true) {
                     }
                 }
             }
-            """.trimIndent(),
-        )
+                """.trimIndent(),
+            )
+        }
 
-        test(
-            {
-                perp.adjustIsolatedMargin("20", AdjustIsolatedMarginInputField.Amount, 0)
-            },
-            """
+        if (perp.staticTyping) {
+            perp.adjustIsolatedMargin(
+                data = "20",
+                type = AdjustIsolatedMarginInputField.Amount,
+                parentSubaccountNumber = 0,
+            )
+
+            assertEquals(InputType.ADJUST_ISOLATED_MARGIN, perp.internalState.input.currentType)
+            assertEquals(20.0, perp.internalState.input.adjustIsolatedMargin.amount)
+            assertEquals(IsolatedMarginInputType.Amount, perp.internalState.input.adjustIsolatedMargin.amountInput)
+            assertEquals(0.018185629293809846, perp.internalState.input.adjustIsolatedMargin.amountPercent)
+
+            val subaccount = perp.internalState.wallet.account.subaccounts[0]
+            assertEquals(100000.0, subaccount?.calculated?.get(CalculationPeriod.current)?.quoteBalance)
+            assertEquals(100020.0, subaccount?.calculated?.get(CalculationPeriod.post)?.quoteBalance)
+            val subaccount1 = perp.internalState.wallet.account.subaccounts[128]
+            assertEquals(500.0, subaccount1?.calculated?.get(CalculationPeriod.current)?.quoteBalance)
+            assertEquals(480.0, subaccount1?.calculated?.get(CalculationPeriod.post)?.quoteBalance)
+        } else {
+            test(
+                {
+                    perp.adjustIsolatedMargin("20", AdjustIsolatedMarginInputField.Amount, 0)
+                },
+                """
             {
                 "input": {
                     "current": "adjustIsolatedMargin",
@@ -315,16 +382,33 @@ class AdjustIsolatedMarginInputTests : V4BaseTests(useParentSubaccount = true) {
                     }
                 }
             }
-            """.trimIndent(),
-        )
+                """.trimIndent(),
+            )
+        }
     }
 
     private fun testMarginAmountPercent() {
-        test(
-            {
-                perp.adjustIsolatedMargin(IsolatedMarginAdjustmentType.Add.name, AdjustIsolatedMarginInputField.Type, 0)
-            },
-            """
+        if (perp.staticTyping) {
+            perp.adjustIsolatedMargin(
+                data = IsolatedMarginAdjustmentType.Add.name,
+                type = AdjustIsolatedMarginInputField.Type,
+                parentSubaccountNumber = 0,
+            )
+
+            assertEquals(InputType.ADJUST_ISOLATED_MARGIN, perp.internalState.input.currentType)
+            assertEquals(IsolatedMarginAdjustmentType.Add, perp.internalState.input.adjustIsolatedMargin.type)
+            assertEquals(null, perp.internalState.input.adjustIsolatedMargin.amount)
+            assertEquals(null, perp.internalState.input.adjustIsolatedMargin.amountPercent)
+        } else {
+            test(
+                {
+                    perp.adjustIsolatedMargin(
+                        IsolatedMarginAdjustmentType.Add.name,
+                        AdjustIsolatedMarginInputField.Type,
+                        0,
+                    )
+                },
+                """
             {
                 "input": {
                     "current": "adjustIsolatedMargin",
@@ -335,14 +419,35 @@ class AdjustIsolatedMarginInputTests : V4BaseTests(useParentSubaccount = true) {
                     }
                 }
             }
-            """.trimIndent(),
-        )
+                """.trimIndent(),
+            )
+        }
 
-        test(
-            {
-                perp.adjustIsolatedMargin("0.1", AdjustIsolatedMarginInputField.AmountPercent, 0)
-            },
-            """
+        if (perp.staticTyping) {
+            perp.adjustIsolatedMargin(
+                data = "0.1",
+                type = AdjustIsolatedMarginInputField.AmountPercent,
+                parentSubaccountNumber = 0,
+            )
+
+            val input = perp.internalState.input.adjustIsolatedMargin
+            assertEquals(IsolatedMarginAdjustmentType.Add, input.type)
+            assertEquals(0.1, input.amountPercent)
+            assertEquals(IsolatedMarginInputType.Percent, input.amountInput)
+            assertEquals(7067.546098618513, input.amount)
+
+            val subaccount = perp.internalState.wallet.account.subaccounts[0]
+            assertEquals(70675.46098618512, subaccount?.calculated?.get(CalculationPeriod.current)?.freeCollateral)
+        } else {
+            test(
+                {
+                    perp.adjustIsolatedMargin(
+                        "0.1",
+                        AdjustIsolatedMarginInputField.AmountPercent,
+                        0,
+                    )
+                },
+                """
             {
                 "wallet": {
                     "account": {
@@ -365,18 +470,31 @@ class AdjustIsolatedMarginInputTests : V4BaseTests(useParentSubaccount = true) {
                     }
                 }
             }
-            """.trimIndent(),
-        )
+                """.trimIndent(),
+            )
+        }
 
-        test(
-            {
-                perp.adjustIsolatedMargin(
-                    IsolatedMarginAdjustmentType.Remove.name,
-                    AdjustIsolatedMarginInputField.Type,
-                    0,
-                )
-            },
-            """
+        if (perp.staticTyping) {
+            perp.adjustIsolatedMargin(
+                data = IsolatedMarginAdjustmentType.Remove.name,
+                type = AdjustIsolatedMarginInputField.Type,
+                parentSubaccountNumber = 0,
+            )
+
+            val input = perp.internalState.input.adjustIsolatedMargin
+            assertEquals(IsolatedMarginAdjustmentType.Remove, input.type)
+            assertEquals(null, input.amount)
+            assertEquals(null, input.amountPercent)
+        } else {
+            test(
+                {
+                    perp.adjustIsolatedMargin(
+                        IsolatedMarginAdjustmentType.Remove.name,
+                        AdjustIsolatedMarginInputField.Type,
+                        0,
+                    )
+                },
+                """
             {
                 "input": {
                     "current": "adjustIsolatedMargin",
@@ -387,14 +505,32 @@ class AdjustIsolatedMarginInputTests : V4BaseTests(useParentSubaccount = true) {
                     }
                 }
             }
-            """.trimIndent(),
-        )
+                """.trimIndent(),
+            )
+        }
 
-        test(
-            {
-                perp.adjustIsolatedMargin("1", AdjustIsolatedMarginInputField.AmountPercent, 0)
-            },
-            """
+        if (perp.staticTyping) {
+            perp.adjustIsolatedMargin(
+                data = "1",
+                type = AdjustIsolatedMarginInputField.AmountPercent,
+                parentSubaccountNumber = 0,
+            )
+
+            val input = perp.internalState.input.adjustIsolatedMargin
+            assertEquals(IsolatedMarginAdjustmentType.Remove, input.type)
+            assertEquals(1.0, input.amountPercent)
+            assertEquals(IsolatedMarginInputType.Percent, input.amountInput)
+            assertEquals(1099.7694760448978, input.amount)
+
+            val subaccount = perp.internalState.wallet.account.subaccounts[128]
+            assertEquals(1132.0151468, subaccount?.calculated?.get(CalculationPeriod.current)?.equity)
+            assertEquals(632.0151467999999, subaccount?.calculated?.get(CalculationPeriod.current)?.notionalTotal)
+        } else {
+            test(
+                {
+                    perp.adjustIsolatedMargin("1", AdjustIsolatedMarginInputField.AmountPercent, 0)
+                },
+                """
             {
                 "wallet": {
                     "account": {
@@ -420,14 +556,35 @@ class AdjustIsolatedMarginInputTests : V4BaseTests(useParentSubaccount = true) {
                     }
                 }
             }
-            """.trimIndent(),
-        )
+                """.trimIndent(),
+            )
+        }
 
-        test(
-            {
-                perp.adjustIsolatedMargin("0.1", AdjustIsolatedMarginInputField.AmountPercent, 0)
-            },
-            """
+        if (perp.staticTyping) {
+            perp.adjustIsolatedMargin(
+                data = "0.1",
+                type = AdjustIsolatedMarginInputField.AmountPercent,
+                parentSubaccountNumber = 0,
+            )
+
+            val input = perp.internalState.input.adjustIsolatedMargin
+            assertEquals(IsolatedMarginAdjustmentType.Remove, input.type)
+            assertEquals(0.1, input.amountPercent)
+            assertEquals(IsolatedMarginInputType.Percent, input.amountInput)
+            assertEquals(109.9769476044898, input.amount)
+
+            val subaccount = perp.internalState.wallet.account.subaccounts[128]
+            assertEquals(1132.0151468, subaccount?.calculated?.get(CalculationPeriod.current)?.equity)
+        } else {
+            test(
+                {
+                    perp.adjustIsolatedMargin(
+                        "0.1",
+                        AdjustIsolatedMarginInputField.AmountPercent,
+                        0,
+                    )
+                },
+                """
             {
                 "wallet": {
                     "account": {
@@ -450,7 +607,8 @@ class AdjustIsolatedMarginInputTests : V4BaseTests(useParentSubaccount = true) {
                     }
                 }
             }
-            """.trimIndent(),
-        )
+                """.trimIndent(),
+            )
+        }
     }
 }
