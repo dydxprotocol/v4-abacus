d2f65e7c4d457e7526a3c47239c17831c3458362
Rui <rui@dydx.exchange>
2024-08-29 18:44:59 +0800

    Field validation

--- src/commonMain/kotlin/exchange.dydx.abacus/validator/transfer/TransferFieldsValidator.kt
+++ src/commonMain/kotlin/exchange.dydx.abacus/validator/transfer/TransferFieldsValidator.kt
@@ -1,5 +1,6 @@
 package exchange.dydx.abacus.validator.transfer
 
+import exchange.dydx.abacus.output.input.TransferType
 import exchange.dydx.abacus.output.input.ValidationError
 import exchange.dydx.abacus.protocols.LocalizerProtocol
 import exchange.dydx.abacus.protocols.ParserProtocol
@@ -21,7 +22,45 @@ internal class TransferFieldsValidator(
         restricted: Boolean,
         environment: V4Environment?
     ): List<ValidationError>? {
-        TODO("Not yet implemented")
+        val errors = mutableListOf<ValidationError>()
+        val transfer = internalState.input.transfer
+        val type = transfer.type ?: return null
+        when (type) {
+            TransferType.deposit, TransferType.withdrawal -> {
+                val usdcSize = parser.asDouble(transfer.size?.usdcSize) ?: 0.0
+                if (usdcSize <= 0.0) {
+                    errors.add(
+                        required(
+                            errorCode = "REQUIRED_SIZE",
+                            field = "size.usdcSize",
+                            actionStringKey = "APP.TRADE.ENTER_AMOUNT",
+                        ),
+                    )
+                }
+                if (transfer.address == null) {
+                    errors.add(
+                        required(
+                            errorCode = "REQUIRED_ADDRESS",
+                            field = "address",
+                            actionStringKey = "APP.DIRECT_TRANSFER_MODAL.ENTER_ETH_ADDRESS",
+                        ),
+                    )
+                }
+            }
+            TransferType.transferOut -> {
+                if (transfer.address == null) {
+                    errors.add(
+                        required(
+                            errorCode = "REQUIRED_ADDRESS",
+                            field = "address",
+                            actionStringKey = "APP.DIRECT_TRANSFER_MODAL.ENTER_ETH_ADDRESS",
+                        ),
+                    )
+                }
+            }
+        }
+
+        return errors
     }
 
     override fun validateTransferDeprecated(
