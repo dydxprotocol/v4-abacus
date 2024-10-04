f5155eff8140bb079394a2b8e79d5fbf115302b3
Rui <102453770+ruixhuang@users.noreply.github.com>
2024-10-04 12:58:47 -0700

    Add on-chain transaction response models (#703)

---
+++ src/commonMain/kotlin/indexer/models/chain/OnChainTransactionResponse.kt
@@ -0,0 +1,64 @@
+package indexer.models.chain
+
+import exchange.dydx.abacus.protocols.asTypedObject
+import exchange.dydx.abacus.utils.Parser
+import kotlinx.serialization.Serializable
+
+// Define the structure of the error message
+@Serializable
+data class ChainError(
+    val message: String,
+    val line: Int? = null,
+    val column: Int? = null,
+    val stack: String? = null
+) {
+    companion object {
+        val unknownError = ChainError(message = "An unknown error occurred", line = null, column = null, stack = null)
+    }
+}
+
+@Serializable
+data class OnChainTransactionErrorResponse(
+    val error: ChainError
+) {
+    companion object {
+        private val parser = Parser()
+
+        fun fromPayload(payload: String?): OnChainTransactionErrorResponse? {
+            return parser.asTypedObject<OnChainTransactionErrorResponse>(payload)
+        }
+    }
+}
+
+// Define the structure of the success message
+@Serializable
+data class ChainEvent(
+    val type: String,
+    val attributes: List<ChainEventAttribute>
+)
+
+@Serializable
+data class ChainEventAttribute(
+    val key: String,
+    val value: String
+)
+
+@Serializable
+data class OnChainTransactionSuccessResponse(
+    val height: Int? = null,
+    val hash: String? = null,
+    val code: Int? = null,
+    val tx: String,
+    val txIndex: Int? = null,
+    val gasUsed: String? = null,
+    val gasWanted: String? = null,
+    val events: List<ChainEvent>? = null,
+) {
+    companion object {
+        private val parser = Parser()
+
+        fun fromPayload(payload: String?): OnChainTransactionSuccessResponse? {
+            return parser.asTypedObject<OnChainTransactionSuccessResponse>(payload)
+        }
+    }
+}
